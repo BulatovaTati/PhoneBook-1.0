69ef9c0c85e2066c321a2f10a7b684e16f409a89
BulatovaTati <kohodatat@gmail.com>
2024-12-03 13:50:23 +0200

    add Profile.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import Profile from './profile/Profile';
+import userData from './profile/userData.json';
+console.log('userData: ', userData);
+
+function App() {
+  return (
+    <>
+      <Profile
+        username={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+}
+export default App;
