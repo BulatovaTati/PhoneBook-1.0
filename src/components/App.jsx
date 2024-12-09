f04c7cfe3ab14cd6c49c42bbde9c9e7982903ec5
BulatovaTati <kohodatat@gmail.com>
2024-12-09 11:19:44 +0200

    add corrections

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,45 +1,28 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import Description from './Description/Description';
 import Feedback from './Feedback/Feedback';
 import Options from './Options/Options';
 import Notification from './Notification/Notification';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+const App = () => {
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem('feedback');
+    return savedFeedback ? JSON.parse(savedFeedback) : { good: 0, neutral: 0, bad: 0 };
+  });
 
-  componentDidMount() {
-    const savedFeedback = JSON.parse(localStorage.getItem('feedback'));
-    if (savedFeedback) {
-      this.setState(savedFeedback);
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('feedback', JSON.stringify(feedback));
+  }, [feedback]);
 
-  componentDidUpdate(_, prevState) {
-    if (prevState !== this.state) {
-      localStorage.setItem('feedback', JSON.stringify(this.state));
-    }
-  }
-
-  updateFeedback = feedbackType => {
-    this.setState(prevState => ({
-      [feedbackType]: prevState[feedbackType] + 1,
+  const updateFeedback = feedbackType => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
     }));
   };
 
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((acc, value) => acc + value, 0);
-  };
-
-  countPositiveFeedback = (totalFeedback, good) => {
-    return Math.round((good / totalFeedback) * 100);
-  };
-
-  handleReset = () => {
-    this.setState(prevState =>
+  const handleReset = () => {
+    setFeedback(prevState =>
       Object.keys(prevState).reduce((acc, key) => {
         acc[key] = 0;
         return acc;
@@ -47,36 +30,41 @@ class App extends Component {
     );
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const options = Object.keys(this.state);
-    const totalFeedback = this.countTotalFeedback();
-    const positiveFeedback = totalFeedback ? this.countPositiveFeedback(totalFeedback, good) : 0;
+  const countTotalFeedback = () => {
+    return Object.values(feedback).reduce((acc, value) => acc + value, 0);
+  };
 
-    return (
-      <>
-        <Description />
-        <Options
-          options={options}
-          onClick={this.updateFeedback}
-          onReset={this.handleReset}
+  const countPositiveFeedback = (totalFeedback, good) => {
+    return totalFeedback ? Math.round((good / totalFeedback) * 100) : 0;
+  };
+
+  const { good, neutral, bad } = feedback;
+  const totalFeedback = countTotalFeedback();
+  const positiveFeedback = countPositiveFeedback(totalFeedback, feedback.good);
+
+  return (
+    <>
+      <Description />
+      <Options
+        options={Object.keys(feedback)}
+        onClick={updateFeedback}
+        onReset={handleReset}
+        totalFeedback={totalFeedback}
+      />
+
+      {totalFeedback > 0 ? (
+        <Feedback
+          good={good}
+          neutral={neutral}
+          bad={bad}
           totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
         />
-
-        {totalFeedback > 0 ? (
-          <Feedback
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            totalFeedback={totalFeedback}
-            positiveFeedback={positiveFeedback}
-          />
-        ) : (
-          <Notification message="No feedback yet" />
-        )}
-      </>
-    );
-  }
-}
+      ) : (
+        <Notification message="No feedback yet" />
+      )}
+    </>
+  );
+};
 
 export default App;
