eeb6fb8e2235fda3fdcfc6ff3fc4e9bccdc745ad
BulatovaTati <kohodatat@gmail.com>
2024-12-13 21:37:16 +0200

    add hw 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,97 @@
-import { useState, useEffect } from 'react';
-import ContactForm from './ContactForm/ContactForm';
-import SearchBox from './SearchBox/SearchBox';
-import ContactList from './ContactList/ContactList';
-import Title from './Title/Title';
-import initContacts from '../assets/contactData.json';
+import { useState, useEffect, useRef, useLayoutEffect } from 'react';
+import { Toaster } from 'react-hot-toast';
+import FetchImages from './API/UnsplashAPI';
+import SearchBar from './SearchBar/SearchBar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import LoadMoreBtn from './LoadMoreBtn/LoadMoreBtn';
+import customToast from './ErrorMessages/Toast/ToastMessage';
+import Loader from './Loader/Loader';
+import ErrorMessage from './ErrorMessages/API/ErrorMessage';
 import s from './App.module.css';
+import ImageModal from './ImageModal/ImageModal';
 
 const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    const savedContacts = localStorage.getItem('contacts');
-    return savedContacts ? JSON.parse(savedContacts) : initContacts;
-  });
-
-  const [filter, setFilter] = useState('');
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isShowButton, setIsShowButton] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const galleryRef = useRef(null);
 
   useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
+    if (!query) return;
 
-  const addContact = newContact => {
-    setContacts(prevContacts => {
-      return [newContact, ...prevContacts];
-    });
+    const renderGallery = async () => {
+      try {
+        setIsLoading(true);
+        const res = await FetchImages(query, page);
+
+        if (res.results.length === 0) {
+          setIsShowButton(false);
+          customToast('warn', 'Sorry, there are no images matching your search');
+          setIsLoading(false);
+          return;
+        }
+
+        setImages(prev => [...prev, ...res.results]);
+        setIsShowButton(page < Math.ceil(res.total / 12) ? true : false);
+        setIsLoading(false);
+      } catch (_) {
+        setError('Something went wrong! Please try again later.');
+        setIsLoading(false);
+      }
+    };
+
+    renderGallery();
+  }, [query, page]);
+
+  const getInputValue = newQuery => {
+    if (newQuery === query) return;
+    setQuery(newQuery);
+    setImages([]);
+    setPage(1);
   };
 
-  const deleteContact = contactId => {
-    setContacts(prevContacts => {
-      return prevContacts.filter(contact => contact.id !== contactId);
-    });
+  const handleImageClick = image => {
+    setSelectedImage(image);
+    setIsModalOpen(true);
   };
 
-  const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  const closeModal = () => {
+    setSelectedImage(null);
+    setIsModalOpen(false);
+  };
+
+  useLayoutEffect(() => {
+    if (galleryRef.current && images.length > 0) {
+      const { height: cardHeight } = galleryRef.current.firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 1,
+        behavior: 'smooth',
+      });
+    }
+  }, [images]);
 
   return (
     <>
-      <Title>Phonebook</Title>
+      <SearchBar onSubmit={getInputValue} />
+      <Toaster />
       <div className={s.container}>
-        <ContactForm contacts={contacts} onAdd={addContact} />
-        <SearchBox value={filter} onSearch={setFilter} />
+        {error ? (
+          <ErrorMessage message={error} />
+        ) : (
+          images.length > 0 && (
+            <ImageGallery images={images} ref={galleryRef} onImageClick={handleImageClick} />
+          )
+        )}
       </div>
-      <ContactList contacts={filteredContacts} onDelete={deleteContact} />
+      {isLoading && <Loader />}
+      {isShowButton && <LoadMoreBtn onClick={() => setPage(page => page + 1)} />}
+      <ImageModal isOpen={isModalOpen} onClose={closeModal} image={selectedImage} />
     </>
   );
 };
