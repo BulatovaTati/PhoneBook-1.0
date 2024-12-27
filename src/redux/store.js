fd8074b481b761f3490d0266e54de2e312b05b7e
BulatovaTati <kohodatat@gmail.com>
2024-12-27 19:01:36 +0200

    add hw 7

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,29 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { persistReducer } from 'redux-persist';
 import { contactsReducer } from './contactsSlice';
 import { filtersReducer } from './filtersSlice';
 
-const persistConfig = {
-  key: 'contacts',
-  storage,
-  blacklist: ['filter'],
-};
-
-export const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
-
 export const store = configureStore({
   reducer: {
-    contacts: persistedContactsReducer,
+    contacts: contactsReducer,
     filter: filtersReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
